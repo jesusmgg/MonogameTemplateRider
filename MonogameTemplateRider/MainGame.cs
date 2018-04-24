86d1ff4201875aa7042c2da7975596505867566e
Jesús González <ito123456789@gmail.com>
2018-04-23 21:32:45 -0400

    Initial commit.

---
+++ MonogameTemplateRider/MainGame.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameTemplateRider
+{
+    public class MainGame : Game
+    {
+        public GraphicsDeviceManager graphicsDevice;
+        public SpriteBatch spriteBatch;
+        
+        public MainGame()
+        {
+            graphicsDevice = new GraphicsDeviceManager(this) {PreferMultiSampling = true};
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
+                ButtonState.Pressed || Keyboard.GetState().IsKeyDown(
+                    Keys.Escape))
+            {
+                Exit();
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Monogame Template Solution for Jetbrains Rider
+
+As Rider (still) does not have an official way of setting up Monogame projects, I created this template to build upon.
+
+What's missing is Asset Pipeline support, as I don't use it. 
+
+
+##### To use the template:
+- Download the solution from the repository.
+- Update the files (and folders) *MonogameTemplateRider.sln*, *MonogameTemplateRider/MonogameTemplateRider.csproj* and *Properties/AssemblyInfo.cs*. Replace every mention of *MonogameTemplateRider* with the desired name for your solution and project.
+- Open the solution file (.sln) with Rider.
+- Right click the solution in the explorer, and the refactor, and adjust namespaces to your project's name.
+
+##### Credits:
+- Jesús González 
